764943ea0c0bf9886bcd9f554e8573d4d925acab
sabebcreative <creative.sabeb@gmail.com>
2016-10-15 22:01:14 +0700

    First Try

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//Scrolling Navbar
+$(window).scroll(function(){
+
+  var Scroll = $(this).scrollTop();
+
+  if(Scroll > 10){
+    $('.navbar').addClass('scrolled');
+  }else {
+    $('.navbar').removeClass('scrolled');
+  };
+
+});
